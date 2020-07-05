0b00860ef62d6384b94327bb2a437b183fb45702
snz <snooooze@gmail.com>
2020-07-05 22:19:57 +0300

    Update b.js

--- b.js
+++ b.js
@@ -1,13 +1,9 @@
 var d = $('<div>',{css:{'width':'100%','height':'100%','margin':0,'position':'absolute','top':0,'z-index':88888,'background-color':'black'}}).appendTo('body');
 var v = $('#videoView').appendTo(d);
 
-var o = true;
 $(v).click(function(){
-	o = o ? false : true;
-	fitScreen(o);
-});
-
-function fitScreen(o){
+	if (typeof o === 'undefined') window.o = true
+	else o = o ? false : true;
 	let w = $(window).width();
 	let h = $(window).height();
 	let r = $('#ResizablePane').height() / $('#ResizablePane').width();
@@ -17,5 +13,4 @@ function fitScreen(o){
 		'margin-top': Math.round(o ? (h-(r*w))/2 : 0) + 'px',
 		'margin-left': Math.round(o ? 0 : (w-(h/r))/2) + 'px'
 	});
-}
-fitScreen(o);
+});
