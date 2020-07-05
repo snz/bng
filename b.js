d3359acf98a35c3e959ac3a0bd31d5eb80816832
snz <snooooze@gmail.com>
2020-07-05 20:22:20 +0300

    Create b.js

---
+++ b.js
@@ -0,0 +1,23 @@
+var d = $('<div>').appendTo('body');z
+var v = $('#videoView').appendTo(d);
+
+d.css({'width':'100%','height':'100%','margin':0,'position':'absolute','top':0,'z-index':88888,'background-color':'black'});
+
+var o = true;
+$(v).click(function(){
+	o = o ? false : true;
+	fitScreen(o);
+});
+
+function fitScreen(o){
+	let w = $(window).width();
+	let h = $(window).height();
+	let r = $('#ResizablePane').height() / $('#ResizablePane').width();
+	$(v).css({
+		'width': Math.round(o ? w : h/r) + 'px',
+		'height': Math.round(o ? r*w : h) + 'px',
+		'margin-top': Math.round(o ? (h-(r*w))/2 : 0) + 'px',
+		'margin-left': Math.round(o ? 0 : (w-(h/r))/2) + 'px'
+	});
+}
+fitScreen(o);
